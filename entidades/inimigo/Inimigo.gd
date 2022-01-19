8584f85e9a537fcdad9bb49e7b1c8bb0cffee114
Joseph Leonardo de olveira <josephleonardodeoliveira@gmail.com>
2022-01-19 12:55:08 -0300

    inimigo programado para seguir o jogador

--- entidades/inimigo/Inimigo.gd
+++ entidades/inimigo/Inimigo.gd
@@ -8,10 +8,12 @@ const SPD = 100
 
 var direction = -1
 var initialpos = self.position
-var pos = Vector2(0,position.y)
+var pos = Vector2(0,position.y);
 
+onready var player = get_node("/root/Main/Player");
 
 func attack():
+	$AnimatedSprite.play("attack")
 	pass
 	
 func receive_damage():
@@ -21,16 +23,30 @@ func dead():
 	pass
 
 func _ready():
-	direction = -1;
-	$FloorDetector.position.x = $CollisionShape2D.shape.get_extents().x * direction; #definindo a direção do raycast
+	direction = 0;
+	$AnimatedSprite.flip_h = !$AnimatedSprite.flip_h;
 	pass
 
 func _process(delta):
-	if is_on_wall() or not $FloorDetector.is_colliding(): #validando se ele n está colidindo com alguma parede ou se vai cair do chao
-		direction = direction * -1
-		$FloorDetector.position.x = $CollisionShape2D.shape.get_extents().x * direction; #definindo a direção do raycast
+	
+	if player and player.position.x - self.position.x < 500 and player.position.x - self.position.x > 70: 
+		direction = 1;
+		$AnimatedSprite.flip_h = false;
+		$AnimatedSprite.play('walk')		
+	elif player and player.position.x - self.position.x >-500 and player.position.x - self.position.x < -70:
+		direction = -1;
+		$AnimatedSprite.flip_h = true;
+		$AnimatedSprite.play('walk')
+	elif player.position.x - self.position.x <-500 or player.position.x - self.position.x > 500:
+		direction = 0;
+		$AnimatedSprite.play('idle');
+	else:
+		attack();
 	
 	pos.y += GRV  #gravidade agindo no inimigo.
 	pos = move_and_slide(pos,Vector2.UP) #movendo o inimigo
 	pos.x = SPD * direction #indicando a posição do inimigo
+	
+	
+	
 	pass
