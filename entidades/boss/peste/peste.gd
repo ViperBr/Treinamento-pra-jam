2969f58e16ed32f0a9cda08f9348570118f49929
Viper <andersonltdsa260@gmail.com>
2022-01-21 17:03:31 -0300

    Melhora peste e adiciona sprites da peste

--- entidades/boss/peste/peste.gd
+++ entidades/boss/peste/peste.gd
@@ -42,28 +42,28 @@ func _ready():
 	pass 
 
 func attack(attack):
-	if attack == 0:
-		attacking = true;
-		sprite.play("dash")
-		timer.disconnect("timeout", self,"set_section_attacks")
-		timer.connect("timeout",self,"set_section_attacks",[state[0]])
-		timer.set_wait_time(1)
-		timer.start()
-		weapon.position.x = 25 * direction
-		pos.x = 200 * direction * velatts
-	elif attack == 1:
-		specialatt = true;
-		sprite.play("attack")
-		weapon.get_child(0).play("att")
-		timer.disconnect("timeout", self,"set_section_attacks")
-		timer.connect("timeout",self,"set_vulnerability")
-		timer.set_wait_time(2)
-		timer.start()
-		#print_debug("ataque especial")
+	if not dead:
+		if attack == 0:
+			attacking = true;
+			sprite.play("dash")
+			timer.disconnect("timeout", self,"set_section_attacks")
+			timer.connect("timeout",self,"set_section_attacks",[state[0]])
+			timer.set_wait_time(1)
+			timer.start()
+			weapon.position.x = 25 * direction
+			pos.x = 200 * direction * velatts
+		elif attack == 1:
+			specialatt = true;
+			sprite.play("attack")
+			weapon.get_child(0).play("att")
+			timer.disconnect("timeout", self,"set_section_attacks")
+			timer.connect("timeout",self,"set_vulnerability")
+			timer.set_wait_time(2)
+			timer.start()
+			#print_debug("ataque especial")
 	pass
 
 func receive_damage(damage):
-	print_debug("ah")
 	if hp - damage >= 0:
 		print_debug("recebi dano!")
 		hp -= damage
@@ -88,64 +88,67 @@ func receive_damage(damage):
 	pass
 
 func dead():
-	sprite.queue_free()
+	dead = true
+	sprite.play("dead")
 	pass
 	
 func set_vulnerability():
-	attacking = false;
-	sprite.play("stun")
-	timer.disconnect("timeout", self,"set_vulnerability")
-	timer.connect("timeout",self,"set_section_attacks",[state[0]])
-	timer.set_wait_time(4)
-	timer.start()
+	if not dead:
+		attacking = false;
+		sprite.play("stun")
+		timer.disconnect("timeout", self,"set_vulnerability")
+		timer.connect("timeout",self,"set_section_attacks",[state[0]])
+		timer.set_wait_time(4)
+		timer.start()
 	pass
 
 func set_section_attacks(att):
-	
-	if att == "att":
-		if hp < 200:
-			velatts = 2;
-			intatts = 1.5
-		if hp < 100:
-			velatts = 3;
-			intatts = 1;
-		if atts < 4:
-			atts += 1;
-			attack(0)
+	if not dead:
+		if att == "att":
+			if hp < 200:
+				velatts = 2;
+				intatts = 1.5
+			if hp < 100:
+				velatts = 3;
+				intatts = 1;
+			if atts < 4:
+				atts += 1;
+				attack(0)
+			else:
+				atts = 0
+				attack(1)
 		else:
-			atts = 0
-			attack(1)
-	else:
-		attacking = false;
-		specialatt = false;
-		reback = false;
-		timer.disconnect("timeout", self,"set_section_attacks")
-		timer.connect("timeout",self,"set_section_attacks",[state[1]])
-		timer.set_wait_time(intatts)
-		timer.start()
-		weapon.get_child(0).play("default")
-		weapon.position.x = 0;
-		pos.x = 0;
-		sprite.play("idle")
-	pass
+			attacking = false;
+			specialatt = false;
+			reback = false;
+			timer.disconnect("timeout", self,"set_section_attacks")
+			timer.connect("timeout",self,"set_section_attacks",[state[1]])
+			timer.set_wait_time(intatts)
+			timer.start()
+			weapon.get_child(0).play("default")
+			weapon.position.x = 0;
+			pos.x = 0;
+			sprite.play("idle")
+		pass
 
 ##função chamada a cada frame
 func _process(delta):
-	if specialatt and not reback and weapon.position.x < 400 and weapon.position.x > -400:
-		weapon.position.x = weapon.position.x + 3 * direction + velatts;
-	elif specialatt and reback:
-		weapon.position.x = weapon.position.x - 6 * direction + velatts;
-	
-	for i in $AreaWeapon.get_overlapping_bodies():
-		if specialatt and reback and weapon.position.x - sprite.position.x > -20 and weapon.position.x - sprite.position.x < 20 and i == sprite.get_parent().get_child(2):
-			receive_damage(25)
+	if not dead:
+		if specialatt and not reback and weapon.position.x < 400 and weapon.position.x > -400:
+			weapon.position.x = weapon.position.x + 3 * direction + velatts;
+		elif specialatt and reback:
+			weapon.position.x = weapon.position.x - 6 * direction + velatts;
+		
+		for i in $AreaWeapon.get_overlapping_bodies():
+			if specialatt and reback and weapon.position.x - sprite.position.x > -20 and weapon.position.x - sprite.position.x < 20 and i == sprite.get_parent().get_child(2):
+				receive_damage(25)
 			
-		if specialatt and i == player and player.attacking and not reback:
-			reback = true
-		if (specialatt or attacking) and i == player and not player.stun_to_hitted and not reback:
-			player.receive_damage(damage)
-			player.poisoning = player.poisoning + 1
-			#print_debug(player.poisoning)
+			if specialatt and i == player and player.attacking and not reback:
+				reback = true
+			if (specialatt or attacking) and i == player and not player.stun_to_hitted and not reback:
+				player.receive_damage(damage)
+				player.poisoning = player.poisoning + 1
+				#print_debug(player.poisoning)
 	
 	pos.y += GRV
 	if player and player.position.x - self.position.x < -12:
