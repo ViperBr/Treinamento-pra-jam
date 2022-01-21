7b02c45f7ea0a5179e377964475cb8ff6d1f7e71
Viper <andersonltdsa260@gmail.com>
2022-01-20 22:06:13 -0300

    Adiciona barra de vida e stamina

--- entidades/player/Interface.gd
+++ entidades/player/Interface.gd
@@ -2,7 +2,9 @@ extends Control
 
 func conectar_vida(vida):
 	get_node("vida").text = "Vida :" + str(vida)
+	get_node("progresso_vida").value = vida
 	
 	
 func conectar_stamina(stamina):
 	get_node("stamina").text = "Stamina :" + str(stamina)
+	get_node("progresso_stamina").value = stamina
